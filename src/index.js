8c389627b6b4b36c633b037efbd1890e2703a638
Kevin Otuteye <kevinotuteye@gmail.com>
2021-05-20 15:25:04 +0000

    Added firebase auth

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import "firebase/auth";
 import React from "react";
 import ReactDOM from "react-dom";
+import { firebaseConfig } from "./auth/firebase.config";
 import Login from "./pages/Login";
 import "./resources/css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login></Login>
+    <FirebaseAuthProvider {...firebaseConfig}>
+      <Login></Login>
+    </FirebaseAuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
