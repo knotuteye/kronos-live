3b8e03f4464e3d6199742d84b92bbf0364241a09
Kevin Otuteye <kevinotuteye@gmail.com>
2021-05-21 16:19:37 +0000

    began styling sidebar tree

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,18 +3,38 @@ import { NavLink } from "react-router-dom";
 
 export default function Sidebar({ menuMapArray }) {
   return (
-    <div className="flex fixed w-72 h-screen bg-gradient-to-br from-blue-500 via-blue-300 to-green-200 shadow-2xl">
-      <div>
+    <div
+      className="flex text-white fixed w-72 h-screen shadow-2xl
+      bg-gradient-to-t from-purple-300 via-purple-500 to-purple-800"
+    >
+      <div className="flex flex-col mt-20 w-full">
         {menuMapArray.map((menuItem) => (
-          <NavLink to={menuItem.path}>
-            <i className={menuItem.icon}></i>
+          <NavLink to={menuItem.path} className="flex px-10 py-3 items-center bg-red-700">
+            {menuItem.icon}
             <p>{menuItem.name}</p>
           </NavLink>
         ))}
       </div>
-      <div className="flex w-full absolute bottom-0 bg-red-600">
-        <i className={""}></i>
-        <p>{"Collapse".toUpperCase()}</p>
+      <div
+        className="flex w-full h-14 absolute bottom-0 cursor-pointer
+        bg-purple-600 bg-opacity-10 justify-center items-center
+        font-bold tracking-widest hover:bg-opacity-100"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 mr-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+          />
+        </svg>
+        <p className="mr-5">{"Collapse".toUpperCase()}</p>
       </div>
     </div>
   );
