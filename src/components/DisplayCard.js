735225512e0a112e937aa691e74d77059caadcc6
Kevin Otuteye <kevinotuteye@gmail.com>
2021-05-24 18:42:11 +0000

    finished DisplayCard styles

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -8,9 +8,9 @@ export default function DisplayCard({ icon, backdrop, data, label, path }) {
       className={`flex rounded-lg px-8 h-32 py-4 shadow-sm hover:shadow-lg w-64 text-white ${backdrop}`}
     >
       <div className="opacity-50">{icon}</div>
-      <div className="flex flex-col flex-1 ml-5">
+      <div className="flex flex-col flex-1 ml-5 mt-2">
         <p className="text-lg opacity-90">{label}</p>
-        <p className="text-4xl text-center mt-3">{data}</p>
+        <p className="text-5xl text-right mt-2">{data}</p>
       </div>
     </Link>
   );
