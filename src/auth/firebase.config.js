07223b97d51e7541617ca425c7959a232df432e1
Kevin Otuteye <kevinotuteye@gmail.com>
2021-08-27 19:03:17 +0000

    Added keys to auth lol

---
+++ src/auth/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDR7_DfgQercNv7WfY3nM1Mf58Dra6_3Sc",
+  authDomain: "kronos-5f460.firebaseapp.com",
+  projectId: "kronos-5f460",
+  storageBucket: "kronos-5f460.appspot.com",
+  messagingSenderId: "215622646875",
+  appId: "1:215622646875:web:bd89e840e5c8936777d37e",
+  measurementId: "G-TXGXC0R325"
+};
